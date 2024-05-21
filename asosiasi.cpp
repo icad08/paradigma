1f5b82b872ef7e6c925d7e27b90786a1fdb62fd9
icad08 <irsyadrm08@gmail.com>
2024-05-21 18:19:54 +0700

    make class dokter and class pasien with the member

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -2,7 +2,20 @@
 #include <vektor>
 using namespace std;
 
-
+class dokter;
+class pasein{ //asosiasi
+public:
+    string nama;
+    vektor<dokter*> daftar_dokter;
+    pasien(string pNama) : nama (pNama){
+        cout << "pasein\"" << nama << "\" tidak ada\n";
+    }
+    ~pasien(){
+        cout << "pasien\"" << nama << "\" tidak ada\n";
+    }
+    void tambahDokter (dokter*);
+    void cetakDokter();
+};
 
 int main(){
 
