ee5b8fe09c8d531107a1443c9f55fa1a8afa44c6
icad08 <irsyadrm08@gmail.com>
2024-05-21 14:49:16 +0700

    add member

--- inicialization.cpp
+++ inicialization.cpp
@@ -7,6 +7,11 @@ class mahasiswa{
     const int id;
     string nama;
     float nilai;
+
+    public:
+    mahasiswa (int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai){ // member 
+        // definisi
+    }
 }
 
 int main()
