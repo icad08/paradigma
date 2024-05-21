773155df7382216250b9cbc6cf7d81a6570261c0
icad08 <irsyadrm08@gmail.com>
2024-05-21 15:28:07 +0700

    membuat manusia mati

--- komposisi/manusia.h
+++ komposisi/manusia.h
@@ -3,5 +3,16 @@
 using namaspace std;
 
 class manusia {
-    
-}
+public: 
+    string name;
+    jantung varJantung;
+
+    manusia (manusia pName) : name(pName){
+
+    }
+    ~manusia() {
+        cout << name <<"mati\n";
+    }
+
+};
+#endif
