4ca725c67cf072c6bd237671bde9bfc455b8ec3e
icad08 <irsyadrm08@gmail.com>
2024-05-21 15:47:05 +0700

    add member dan perintah

--- agregasi/anak.h
+++ agregasi/anak.h
@@ -2,5 +2,13 @@
 #define ANAK_H
 
 class anak {
-    
-}
+    public:
+    string nama;
+    anak (string pNama) :nama(pNama){
+        cout << "anak\"" << nama << "\" ada\n";
+    }
+    ~anak (){
+        cout << "anak \"" << nama << "\" hilang\n";
+    }
+};
+#endif
